c6d58de1e08c70b1ef5238079c948afbb6fea646
max cheung <maxcheung@optusnet.com.au>
2016-10-22 11:35:31 +1100

    initial

---
+++ prototypedemo/src/main/java/prototypedemo/prototypedemo/Rectangle.java
@@ -0,0 +1,13 @@
+package prototypedemo.prototypedemo;
+
+public class Rectangle extends Shape {
+
+	public Rectangle() {
+		type = "Rectangle";
+	}
+
+	@Override
+	public void draw() {
+		System.out.println("Inside Rectangle::draw() method.");
+	}
+}
